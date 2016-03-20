015a06c12ca8d3412d0c3e06981ec75725a5fef5
lee715 <li.l@huiyi-tech.com>
2016-03-21 00:43:57 +0800

    update sneaky

--- Skyfile.js
+++ Skyfile.js
@@ -1,7 +1,7 @@
 'use strict'
 /*global sneaky*/
 
-sneaky('dev', function () {
+sneaky('release', function () {
   this.description = 'Deploy to dev environment'
   this.user = 'root'
   this.host = '139.196.8.138'
@@ -20,10 +20,32 @@ sneaky('dev', function () {
 + app.js
 - *
 `
-
   this.before('rm -rf lib && coffee -o lib -c src')
-  this.after('pm2 restart app')
   this.overwrite = true
   this.nochdir = true
+})
 
+sneaky('dev', function () {
+  this.description = 'Deploy to dev environment'
+  this.user = 'root'
+  this.host = '139.196.8.138'
+  this.path = '~/services/anmoyi_test'
+  this.filter = `
++ config
++ config/default.json
++ lib**
++ pm2**
++ public
++ public/templates**
++ public/tmp**
++ public/bower
++ public/bower/jquery**
++ package.json
++ app.js
+- *
+`
+
+  this.before('rm -rf lib && coffee -o lib -c src')
+  this.overwrite = true
+  this.nochdir = true
 })
