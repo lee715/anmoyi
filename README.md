87d97be7c0af7ef6dacf65378acdf53f221106d3
lee715 <lei@teambition.com>
2017-12-14 16:46:32 +0800

    new pay

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 Teambition Report Service
 ====================
 
+环境
+node
+bower
+npm i sneaky
+npm i gulp
+
+前端编译
+bower update && gulp
+
+代码发布
+npm i && sneaky d ssb
--- Skyfile.js
+++ Skyfile.js
@@ -1,78 +1,6 @@
 'use strict'
 /*global sneaky*/
 
-sneaky('release', function () {
-  this.description = 'Deploy to dev environment'
-  this.user = 'root'
-  this.host = '139.196.8.138'
-  this.path = '/services/anmoyi'
-  this.filter = `
-+ config
-+ config/default.json
-+ lib**
-+ pm2**
-+ public
-+ public/templates**
-+ public/tmp**
-+ public/bower
-+ public/bower/jquery**
-+ package.json
-+ app.js
-- *
-`
-  this.before('rm -rf lib && coffee -o lib -c src && cp src/services/socket.js lib/services/socket.js')
-  this.overwrite = true
-  this.nochdir = true
-})
-
-sneaky('onip', function () {
-  this.description = 'Deploy to dev environment'
-  this.user = 'root'
-  this.host = '139.196.176.248'
-  this.path = '~/services/core'
-  this.filter = `
-+ config
-+ config/default.json
-+ lib**
-+ pm2**
-+ public
-+ public/templates**
-+ public/tmp**
-+ public/bower
-+ public/bower/jquery**
-+ package.json
-+ app.js
-- *
-`
-  this.before('rm -rf lib && coffee -o lib -c src')
-  this.overwrite = true
-  this.nochdir = true
-})
-
-sneaky('ananbei', function () {
-  this.description = 'Deploy to dev environment'
-  this.user = 'root'
-  this.host = '39.108.104.138'
-  this.path = '~/server/'
-  this.filter = `
-+ config
-+ config/default.json
-+ lib**
-+ pm2**
-+ public
-+ public/templates**
-+ public/tmp**
-+ public/bower
-+ public/bower/jquery**
-+ package.json
-+ app.js
-- *
-`
-  this.before('rm -rf lib && coffee -o lib -c src')
-  this.overwrite = true
-  this.nochdir = true
-})
-
 sneaky('ssb', function () {
   this.description = 'Deploy to dev environment'
   this.user = 'root'
@@ -96,55 +24,6 @@ sneaky('ssb', function () {
 `
   this.before('rm -rf lib && coffee -o lib -c src && cp src/services/socket.js lib/services/socket.js')
   this.after('pm2 restart app')
-  this.overwrite = true
-  this.nochdir = true
-})
-
-sneaky('xrg', function () {
-  this.description = 'Deploy to dev environment'
-  this.user = 'root'
-  this.host = '139.224.130.230'
-  this.path = '~/server/'
-  this.filter = `
-+ config
-+ config/default.json
-+ lib**
-+ pm2**
-+ public
-+ public/templates**
-+ public/tmp**
-+ public/bower
-+ public/bower/jquery**
-+ package.json
-+ app.js
-- *
-`
-  this.before('rm -rf lib && coffee -o lib -c src')
-  this.overwrite = true
-  this.nochdir = true
-})
-
-sneaky('dev', function () {
-  this.description = 'Deploy to dev environment'
-  this.user = 'root'
-  this.host = '139.196.8.138'
-  this.path = '~/services/anmoyi_test'
-  this.filter = `
-+ config
-+ config/default.json
-+ lib**
-+ pm2**
-+ public
-+ public/templates**
-+ public/tmp**
-+ public/bower
-+ public/bower/jquery**
-+ package.json
-+ app.js
-- *
-`
-
-  this.before('rm -rf lib && coffee -o lib -c src')
   this.overwrite = true
   this.nochdir = true
 })
